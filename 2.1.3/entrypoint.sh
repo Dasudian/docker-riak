21258ad50ddeb75801edd5addc5a387d1c4236f2
Mengz <mz@dasudian.com>
2016-03-30 15:47:30 +0800

    add cluster functions

---
+++ 2.1.3/entrypoint.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -m
+
+RIAK_CONFIG="/etc/riak/riak.conf"
+
+SEDARG="-i 's/%STORAGE_BACKEND%/${STORAGE_BACKEND}/' ${RIAK_CONFIG}"
+eval ${SEDARG}
+
+NODE_IP=$(ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
+SEDARG="-i 's/%NODE_IP%/${NODE_IP}/' ${RIAK_CONFIG}"
+eval ${SEDARG}
+
+supervisord -n &
+
+echo "Start the container."
+
+if [ ! -z ${CLUSTER_NODE} ]; then
+    while :
+    do
+        sleep 5
+        RT=$(su - riak -c "/usr/sbin/riak ping")
+        if [ ${RT} = "pong" ]; then
+            su - riak -c "/usr/sbin/riak-admin cluster join ${CLUSTER_NODE}"
+            if [ $? -eq 0 ]; then
+                if [ "$LAST_NODE" = "yes" ]; then
+                    sleep 5
+                    su - riak -c "/usr/sbin/riak-admin cluster plan"
+                    su - riak -c "/usr/sbin/riak-admin cluster commit"
+                fi
+                break
+            else
+                echo "Join cluster to ${CLUSTER_NODE} failed."
+                break
+            fi
+        fi
+    done
+fi
+
+fg %1
