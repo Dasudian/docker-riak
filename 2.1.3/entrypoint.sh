6d493fc52392ce38aae2cc4c53c2f3c427b79f04
Mengz <mz@dasudian.com>
2016-03-31 15:32:51 +0800

    update entrypoint script
    do not join the cluster again when start the container

--- 2.1.3/entrypoint.sh
+++ 2.1.3/entrypoint.sh
@@ -19,18 +19,26 @@ if [ ! -z ${CLUSTER_NODE} ]; then
     do
         sleep 5
         RT=$(su - riak -c "/usr/sbin/riak ping")
-        if [ ${RT} = "pong" ]; then
-            su - riak -c "/usr/sbin/riak-admin cluster join ${CLUSTER_NODE}"
+        if [ "${RT}" = "pong" ]; then
+            riak-admin cluster status | grep ${CLUSTER_NODE}
             if [ $? -eq 0 ]; then
-                if [ "$LAST_NODE" = "yes" ]; then
-                    sleep 5
-                    su - riak -c "/usr/sbin/riak-admin cluster plan"
-                    su - riak -c "/usr/sbin/riak-admin cluster commit"
-                fi
+                echo "Already in the cluster."
                 break
             else
-                echo "Join cluster to ${CLUSTER_NODE} failed."
-                break
+                echo "Join the cluster."
+                riak-admin cluster join ${CLUSTER_NODE}
+                if [ $? -eq 0 ]; then
+                    if [ "$LAST_NODE" = "yes" ]; then
+                        sleep 5
+                        echo "Commit the plan."
+                        riak-admin cluster plan
+                        riak-admin cluster commit
+                    fi
+                    break
+                else
+                    echo "Join cluster to ${CLUSTER_NODE} failed."
+                    break
+                fi
             fi
         fi
     done
