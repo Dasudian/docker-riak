5f9c17cc54b3fed949fef09f93f9c4f561850305
Mengz <mz@dasudian.com>
2016-04-08 11:25:16 +0800

    remove the placeholder in riak.conf, update the entrypoint

--- 2.1.3/entrypoint.sh
+++ 2.1.3/entrypoint.sh
@@ -2,13 +2,24 @@
 set -m
 
 RIAK_CONFIG="/etc/riak/riak.conf"
+# RIAK_CONFIG="riak.conf"
+# STORAGE_BACKEND="leveldb"
 
-SEDARG="-i 's/%STORAGE_BACKEND%/${STORAGE_BACKEND}/' ${RIAK_CONFIG}"
-eval sed ${SEDARG}
+storage_backend=$(egrep "^storage_backend = .*" ${RIAK_CONFIG} | awk -F' = ' '{print $2}')
+if [ "${storage_backend}" != "${STORAGE_BACKEND}" ];then
+    echo "Set riak storage backend to ${STORAGE_BACKEND}"
+    SEDARG="-i 's/^storage_backend = .*/storage_backend = ${STORAGE_BACKEND}/' ${RIAK_CONFIG}"
+    eval sed ${SEDARG}
+fi
 
 NODE_IP=$(ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
-SEDARG="-i 's/%NODE_IP%/${NODE_IP}/' ${RIAK_CONFIG}"
-eval sed ${SEDARG}
+# NODE_IP="127.0.0.1"
+grep "nodename = riak@${NODE_IP}" ${RIAK_CONFIG}
+if [ $? -ne 0 ];then
+    echo "Set riak nodename to riak@${NODE_IP}"
+    SEDARG="-i 's/^nodename = riak@.*/nodename = riak@${NODE_IP}/' ${RIAK_CONFIG}"
+    eval sed ${SEDARG}
+fi
 
 supervisord -n &
 
@@ -18,7 +29,7 @@ if [ ! -z ${CLUSTER_NODE} ]; then
     while :
     do
         sleep 5
-        RT=$(su - riak -c "/usr/sbin/riak ping")
+        RT=$(riak ping)
         if [ "${RT}" = "pong" ]; then
             riak-admin cluster status | grep ${CLUSTER_NODE}
             if [ $? -eq 0 ]; then
