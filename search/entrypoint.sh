84cfaa21be8eebfd996dd2e9ac465d860631b6da
Mengz <mz@dasudian.com>
2016-06-27 12:52:57 +0800

    set the nodename for standalone and cluster
    add ENV NODE_HOST

--- search/entrypoint.sh
+++ search/entrypoint.sh
@@ -4,13 +4,18 @@ set -m
 RIAK_CONFIG="/etc/riak/riak.conf"
 
 C_IP=$(ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
-# NODE_IP="127.0.0.1"
-grep "nodename = riak@$C_IP" ${RIAK_CONFIG}
-if [ $? -ne 0 ];then
-    echo "Set riak nodename to riak@$C_IP"
-    sed -ri "s|nodename = .*|nodename = riak@$C_IP|" $RIAK_CONFIG
+
+if [[ $NODE_HOST =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]];then
+  echo "Use the IP as the NODE_HOST, the nodename of riak is riak@$NODE_HOST"
+else
+  grep -E "^$C_IP $NODE_HOST$" /etc/hosts
+  if [ $? -ne 0 ];then
+    echo "$C_IP $NODE_HOST" >> /etc/hosts
+    echo "Use a HOST NAME as the NODE_HOST, the nodename of riak is riak@$NODE_HOST"
+  fi
 fi
 
+sed -ri "s|nodename = .*|nodename = riak@$NODE_HOST|" $RIAK_CONFIG
 sed -ri "s|storage_backend = .*|storage_backend = $STORAGE_BACKEND|" $RIAK_CONFIG
 
 if [ "$1" = "supervisord" ]; then
